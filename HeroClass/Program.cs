6e5b1aa486364604fc59a535d3552c9d5eb1f4c0
HanChengLiang <James_61111@live.ca>
2015-01-30 23:21:58 -0500

    added constuctor

---
+++ HeroClass/Program.cs
@@ -0,0 +1,15 @@
+﻿// Author: Han Cheng Liang
+// ID: 300791705
+
+using System;
+
+namespace Hero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
---
+++ HeroClass/hero.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroClass
+{
+
+        class Hero
+    {
+        //private property
+        private int strength;
+
+        private int speed;
+
+        private int health;
+
+
+
+        //public property
+        public string name;
+
+            
+    
+            
+        //constuctor
+        public Hero(string name)
+        {
+            this.name = name;
+            this.generateAbilities();
+        }
+
+        private void generateAbilities()
+        {
+            Random number = new Random();
+            this.strength = number.Next(100) + 1;
+            this.speed = number.Next(100) + 1;
+            this.health = number.Next(100) + 1;
+        }
+
+        public void fight()
+        {
+            if (this.hitAttempt())
+            {
+                int damageRate = this.hitDamage();
+                Console.WriteLine("Attack success, {0} damage to ememy", damageRate);
+
+
+            }
