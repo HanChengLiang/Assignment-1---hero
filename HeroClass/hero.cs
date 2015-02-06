37cab7df623f8177dfebbd74cc6ba20a17625e15
HanChengLiang <James_61111@live.ca>
2015-02-06 12:54:55 -0500

    Added superhero class

--- HeroClass/hero.cs
+++ HeroClass/hero.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Author: Han Cheng Liang
+// ID: 300791705
+using System;
 
-namespace HeroClass
+
+namespace Hero
 {
 
         class Hero
